546ba39cc746f3c66ac8e3e75c9450489824d5c9
arkceajin <arkceajin@gmail.com>
2018-06-05 18:37:37 +0900

    Update paintEvent.h

--- paintEvent/paintEvent.h
+++ paintEvent/paintEvent.h
@@ -17,20 +17,23 @@ public:
     void mouseMoveEvent(QMouseEvent *e) override
     {
         rect = QRect(startPt, e->pos()).normalized();
+        qDebug()<<"start paint";
+        repaint(rect.adjusted(-5,-5,5,5)); // adjusted to include stroke
+        grab().save(QString::number(++i) + ".PNG", "PNG");
+        qDebug()<<"end paint";
+    }
 
-        QPixmap pixmap(size());
-        pixmap.fill(Qt::white);
-        QPainter p(&pixmap);
+    void paintEvent(QPaintEvent *event) override
+    {
+        QPainter p(this);
 
         p.setBrush(Qt::NoBrush);
         p.setPen(QPen(Qt::black, 10));
         p.drawRect(rect);
-
-        setPixmap(pixmap);
-        repaint(rect.adjusted(-5,-5,5,5)); // adjusted to include stroke
     }
 
 private:
     QRect rect;
     QPoint startPt;
+    int i = 0;
 };
